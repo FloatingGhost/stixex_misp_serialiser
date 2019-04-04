b44fee03934c39e6bbc967574de214ead23d31e2
Hannah Ward <Hannah.ward9001@gmail.com>
2019-04-04 16:11:38 +0100

    Add default values for MISP event

--- lib/stixex/serialiser/misp.ex
+++ lib/stixex/serialiser/misp.ex
@@ -6,7 +6,6 @@ defmodule StixEx.Serialiser.MISP do
   @behaviour StixEx.Serialiser
 
   require Logger
-  alias MISP.Attribute
 
   defp convert(stix_object)
 
@@ -19,17 +18,35 @@ defmodule StixEx.Serialiser.MISP do
   defp convert([]), do: []
 
   defp convert(%{type: "bundle", id: "bundle--" <> id, objects: objects}) do
-    %MISP.Event{
-      Event: %MISP.EventInfo{
+    attrs = convert(objects)
+
+    time =
+      DateTime.utc_now()
+      |> DateTime.to_unix()
+      |> Kernel.to_string()
+
+    %{
+      Event: %{
         uuid: id,
         info: "From STIX",
-        Attribute: convert(objects)
+        publish_timestamp: time,
+        published: true,
+        distribution: 3,
+        analysis: 0,
+        attribute_count: Enum.count(attrs),
+        Org: %{},
+        OrgC: %{},
+        Galaxy: [],
+        ShadowAttribute: [],
+        RelatedEvent: [],
+        Tag: [],
+        Attribute: attrs
       }
     }
   end
 
   defp convert(%{type: "mutex", name: name, id: "mutex--" <> id}),
-    do: %Attribute{type: "mutex", value: name, uuid: id}
+    do: %{type: "mutex", value: name, uuid: id}
 
   defp convert(%{type: "windows_registry_key", key: key, values: values}) do
     if Enum.count(values) > 0 do
@@ -40,15 +57,15 @@ defmodule StixEx.Serialiser.MISP do
   end
 
   defp convert(%{type: "mac-addr", value: value, id: "mac-addr--" <> id}) do
-    %Attribute{type: "mac-address", value: value, uuid: id}
+    %{type: "mac-address", value: value, uuid: id}
   end
 
   defp convert(%{type: "email-addr", value: value, id: "email-addr--" <> id}) do
-    %Attribute{type: "email-dst", value: value, uuid: id}
+    %{type: "email-dst", value: value, uuid: id}
   end
 
   defp convert(%{type: "domain-name", value: value, id: "domain-name--" <> id}) do
-    %Attribute{type: "domain", value: value, uuid: id}
+    %{type: "domain", value: value, uuid: id}
   end
 
   defp convert(%{type: "x509-certificate", hashes: hashes}) when not is_nil(hashes) do
@@ -57,54 +74,54 @@ defmodule StixEx.Serialiser.MISP do
 
   defp convert(%{type: "file", name: name, hashes: hashes, id: "file--" <> id}) do
     [
-      %Attribute{type: "filename", value: name, uuid: id},
+      %{type: "filename", value: name, uuid: id},
       get_common_hashes(hashes, type_prefix: "filename|", value_prefix: "#{name}|")
     ]
   end
 
   defp convert(%{type: "ipv6-addr", value: value, id: "ipv6-addr--" <> id}) do
-    %Attribute{type: "ip-dst", value: value, uuid: id}
+    %{type: "ip-dst", value: value, uuid: id}
   end
 
   defp convert(%{type: "user-account", user_id: value, id: "user-account--" <> id}) do
-    %Attribute{type: "target-user", value: value, uuid: id}
+    %{type: "target-user", value: value, uuid: id}
   end
 
   defp convert(%{type: "ipv4-addr", value: value, id: "ipv4-addr--" <> id}) do
-    %Attribute{type: "ip-dst", value: value, uuid: id}
+    %{type: "ip-dst", value: value, uuid: id}
   end
 
   defp convert(%{type: "url", value: value, id: "url--" <> id}) do
-    %Attribute{type: "url", value: value, uuid: id}
+    %{type: "url", value: value, uuid: id}
   end
 
   defp convert(%{type: "autonomous-system", number: value, id: "autonomous-system--" <> id}) do
-    %Attribute{type: "AS", value: value, uuid: id}
+    %{type: "AS", value: value, uuid: id}
   end
 
   defp convert(%{type: "indicator", pattern: pattern, id: "indicator--" <> id}) do
-    %Attribute{type: "stix2-pattern", value: pattern, uuid: id}
+    %{type: "stix2-pattern", value: pattern, uuid: id}
   end
 
   defp convert(%{type: "campaign", name: name, id: "campaign--" <> id}) do
-    %Attribute{type: "campaign-name", value: name, uuid: id}
+    %{type: "campaign-name", value: name, uuid: id}
   end
 
   defp convert(%{type: "vulnerability", name: name, id: "vulnerability--" <> id}) do
-    %Attribute{type: "vulnerability", value: name, uuid: id}
+    %{type: "vulnerability", value: name, uuid: id}
   end
 
   defp convert(%{type: "identity", name: name, id: "identity--" <> id}) do
-    %Attribute{type: "first-name", value: name, uuid: id}
+    %{type: "first-name", value: name, uuid: id}
   end
 
   defp convert(%{type: "threat-actor", name: name, aliases: aliases, id: "threat-actor--" <> id}) do
     [
-      %Attribute{type: "threat-actor", value: name, uuid: id},
+      %{type: "threat-actor", value: name, uuid: id},
       if is_nil(aliases) do
         []
       else
-        Enum.map(aliases, &%Attribute{type: "threat-actor", value: &1})
+        Enum.map(aliases, &%{type: "threat-actor", value: &1})
       end
     ]
   end
@@ -129,7 +146,7 @@ defmodule StixEx.Serialiser.MISP do
     ]
     |> Enum.filter(fn {_, x} -> not is_nil(x) end)
     |> Enum.map(fn {h, x} ->
-      %Attribute{type: "#{opts[:type_prefix]}#{h}", value: "#{opts[:value_prefix]}#{x}"}
+      %{type: "#{opts[:type_prefix]}#{h}", value: "#{opts[:value_prefix]}#{x}"}
     end)
   end
 
